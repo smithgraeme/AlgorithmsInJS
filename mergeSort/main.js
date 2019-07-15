f06f1fc39d07c0b1c41ae2c3dccfec248456c667
Graeme Smith <30961431+smithgraeme@users.noreply.github.com>
2019-07-14 19:29:39 -0600

    Restructure mergesort folder

---
+++ mergeSort/main.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+const mergeSort = require('./MergeSort')
+
+log = input => console.log(input)
+
+const size = 100
+const inputArray = Array(size).fill(0).map(() => Math.floor((Math.random() - 0.5) * 10000))
+const expectedOutput = inputArray.slice().sort(function(a, b){return a-b})
+
+mergeSort(inputArray)
+
+assert(inputArray.toString() == expectedOutput.toString())
+log(`\nResult:\n${inputArray}\n\nExpected:\n${expectedOutput}`)
